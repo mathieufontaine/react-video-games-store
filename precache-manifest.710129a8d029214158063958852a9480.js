84c0a8945d50206cb4357364dff50feaff5217b4
Mathieu Fontaine <mathieu.antoine.fontaine@gmail.com>
2020-07-10 20:59:11 +0200

    Updates

---
+++ precache-manifest.710129a8d029214158063958852a9480.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62c2106c19c17c10d7a3bec57ab9dfb",
+    "url": "/react-video-games-store/index.html"
+  },
+  {
+    "revision": "8727d7587e4ced894640",
+    "url": "/react-video-games-store/static/css/2.ddd52fb8.chunk.css"
+  },
+  {
+    "revision": "6780f884f207a8260a52",
+    "url": "/react-video-games-store/static/css/main.9829fcce.chunk.css"
+  },
+  {
+    "revision": "8727d7587e4ced894640",
+    "url": "/react-video-games-store/static/js/2.d57d34e9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-video-games-store/static/js/2.d57d34e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6780f884f207a8260a52",
+    "url": "/react-video-games-store/static/js/main.1de09373.chunk.js"
+  },
+  {
+    "revision": "fb99127d9afa1ab59406",
+    "url": "/react-video-games-store/static/js/runtime-main.f5541d92.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-video-games-store/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-video-games-store/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-video-games-store/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-video-games-store/static/media/slick.f97e3bbf.svg"
+  }
+]);
